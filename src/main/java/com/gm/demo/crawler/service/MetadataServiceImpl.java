fc5a4012e176bc9f966b8dfe59883ea47b9d9778
Jason <admin@gm.com>
2018-12-20 15:35:24 +0800

    优化代码

--- src/main/java/com/gm/demo/crawler/service/MetadataServiceImpl.java
+++ src/main/java/com/gm/demo/crawler/service/MetadataServiceImpl.java
@@ -10,6 +10,7 @@ import com.gm.utils.base.Bool;
 import com.gm.utils.base.Convert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -17,28 +18,46 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
+ * The type Metadata service.
+ *
  * @author Jason
  */
 @Service
 public class MetadataServiceImpl {
 
+    /**
+     * The Tab mapper.
+     */
     @Autowired
     TabMapper tabMapper;
 
+    /**
+     * The Metadata mapper.
+     */
     @Autowired
     MetadataMapper metadataMapper;
 
+    /**
+     * The Metadata mapper ext.
+     */
     @Autowired
     MetadataMapperExt metadataMapperExt;
 
+    /**
+     * Gets tab.
+     *
+     * @param tab the tab
+     * @return the tab
+     */
     public List<Metadata> getTab(String tab) {
         return metadataMapperExt.getTab(tab);
     }
 
     /**
      * 保存元数据
-     * @param req
-     * @return
+     *
+     * @param req the req
+     * @return integer
      */
     @Transactional(rollbackFor = Exception.class)
     public Integer save(SaveMetadataReq req) {
@@ -47,7 +66,7 @@ public class MetadataServiceImpl {
         Map<String, Integer> map = data.stream().collect(Collectors.toMap(Metadata::getField, Metadata::getId));
         // 不存在表先创建
         if (Bool.isNull(data)) {
-            tabMapper.createTab(req.getTab());
+            createTab(req);
         }
         // 字段存在就更改
         if (map.containsKey(req.getField()) || map.containsKey(req.getOldField())){
@@ -66,14 +85,32 @@ public class MetadataServiceImpl {
     }
 
     /**
+     * 创建表.
+     *
+     * @param req the req
+     */
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
+    public void createTab(SaveMetadataReq req) {
+        Metadata id = new Metadata();
+        id.setField("id");
+        id.setDataType("int");
+        id.setLen(11);
+        id.setComment("主键");
+        id.setTab(req.getTab());
+        metadataMapper.insertSelective(id);
+        tabMapper.createTab(req.getTab());
+    }
+
+    /**
      * 删除表
-     * @param tab
-     * @return
+     *
+     * @param tab the tab
+     * @return boolean
      */
     @Transactional(rollbackFor = Exception.class)
     public Boolean dropTab(String tab) {
-        tabMapper.dropTab(tab);
         metadataMapperExt.dropTab(tab);
+        tabMapper.dropTab(tab);
         return true;
     }
 }
