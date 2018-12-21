e461ee1f61f569ab91348e0d5f31b2ba877655ac
Jason <admin@gm.com>
2018-12-21 20:03:36 +0800

    可以了 .

--- src/main/java/com/gm/demo/crawler/controller/MtController.java
+++ src/main/java/com/gm/demo/crawler/controller/MtController.java
@@ -2,11 +2,16 @@ package com.gm.demo.crawler.controller;
 
 import com.gm.demo.crawler.entity.req.CrawlReq;
 import com.gm.demo.crawler.service.MtServiceImpl;
+import com.gm.help.base.Quick;
 import com.gm.model.response.HttpResult;
 import com.gm.model.response.JsonResult;
+import com.gm.utils.base.ExceptionUtils;
+import com.gm.utils.base.Logger;
+import com.gm.utils.ext.Web;
 import com.gm.utils.third.Http;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,20 +28,63 @@ import javax.validation.Valid;
 @RequestMapping("mt")
 public class MtController {
 
+    public static final String offset = "offset";
+    public static final String pageSize = "pageSize";
+
     @Autowired
     MtServiceImpl mtService;
 
     @PostMapping("entry")
     @ApiOperation(value = "释放一只爬虫")
     public JsonResult entry(@RequestBody @Valid CrawlReq req) {
-        HttpResult result = Http.doGet(req.getUrl(), req.getHeaders(), req.getParams());
-        if(!JsonResult.SUCCESS.equals(result.getStatus())){
-            result = Http.doPost(req.getUrl(), req.getHeaders(), req.getParams());
-            if(!JsonResult.SUCCESS.equals(result.getStatus())){
-                JsonResult.unsuccessful(new String(result.getResult()));
+        String url = req.getUrl();
+        Integer start = Integer.parseInt(Web.getParam(url, offset));
+        Integer size = Integer.parseInt(Web.getParam(url, pageSize));
+        P page = new P(start, 0, size);
+        // 统计本次爬取总记录数
+        Integer[] sum = {0};
+        Quick.echo(x -> {
+            url.replace(
+                    offset.concat("=").concat(page.oldStart.toString()),
+                    offset.concat("=").concat(page.newStart.toString())
+            );
+            HttpResult result = Http.doGet(url, req.getHeaders(), req.getParams());
+            if (!JsonResult.SUCCESS.equals(result.getStatus())) {
+                result = Http.doPost(req.getUrl(), req.getHeaders(), req.getParams());
+                if (!JsonResult.SUCCESS.equals(result.getStatus())) {
+                    JsonResult.unsuccessful(new String(result.getResult()));
+                }
             }
+            String json = new String(result.getResult());
+            Integer count = mtService.handler(json);
+            sum[0] += count;
+            if (count < page.getPageSize()) {
+                // 收集到第gather条
+                Logger.debug("stop:   ".concat(url).concat("   \ngather:    ").concat(sum[0].toString()));
+                // 没有更多数据了结束
+                ExceptionUtils.cast();
+            } else {
+                Logger.debug("finish:   ".concat(url).concat("   \ngather:    ").concat(count.toString()));
+                // 从这里开始
+                page.setNewStart(page.getOldStart() + page.getPageSize());
+            }
+        });
+        return JsonResult.as(sum[0]);
+    }
+
+    /**
+     * 分页对象
+     */
+    @Data
+    static class P {
+        private Integer oldStart;
+        private Integer newStart;
+        private Integer pageSize;
+
+        public P(Integer oldStart, Integer newStart, Integer pageSize) {
+            this.oldStart = oldStart;
+            this.newStart = newStart;
+            this.pageSize = pageSize;
         }
-        String json = new String(result.getResult());
-        return JsonResult.as(mtService.handler(json));
     }
 }
