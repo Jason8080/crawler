127711947eceb2443bb119244df55b27691e1ad2
Jason <admin@gm.com>
2019-01-02 15:32:45 +0800

    添加标题.

--- src/main/java/com/gm/demo/crawler/service/LunaticCrawlerServiceImpl.java
+++ src/main/java/com/gm/demo/crawler/service/LunaticCrawlerServiceImpl.java
@@ -4,7 +4,6 @@ import com.gm.demo.crawler.dao.mapper.TabMapper;
 import com.gm.demo.crawler.dao.model.Gather;
 import com.gm.enums.Regexp;
 import com.gm.utils.ext.Regex;
-import com.gm.utils.ext.Web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +25,16 @@ public class LunaticCrawlerServiceImpl extends CrawlerServiceImpl {
     MetadataServiceImpl metadataService;
 
     public Integer handler(Gather gather, String url, String html) {
+        String title = Regex.findFirst(html, Regexp.FIND_HTML_TITLE.getCode());
+        title = title.substring("<title>".length(), title.length() - "</title>".length());
         List<String> mobiles = Regex.find(html, Regexp.FIND_MOBILE.getCode());
         mobiles = mobiles.stream().distinct().collect(Collectors.toList());
         List<Map<String, Object>> maps = new ArrayList();
         for (String mobile : mobiles) {
             Map<String, Object> map = new HashMap(0);
-            map.put("url", url);
+            map.put("title", title);
             map.put("mobile", mobile);
+            map.put("url", url);
             maps.add(map);
         }
         return handler(gather.getTab(), maps, gather.getFilters().split(","));
