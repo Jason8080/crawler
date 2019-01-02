10ae90b1fd2830550b3194252a2a8864032b4ded
Jason <admin@gm.com>
2019-01-02 18:37:25 +0800

    前后弄个非数字.

--- src/main/java/com/gm/demo/crawler/service/LunaticCrawlerServiceImpl.java
+++ src/main/java/com/gm/demo/crawler/service/LunaticCrawlerServiceImpl.java
@@ -30,7 +30,7 @@ public class LunaticCrawlerServiceImpl extends CrawlerServiceImpl {
         String title = Convert.toEmpty(Regex.findFirst(html, Regexp.FIND_HTML_TITLE.getCode()), "<title></title>");
         title = title.substring("<title>".length(), title.length() - "</title>".length());
         List<String> mobiles = Regex.find(html, "([^\\d])((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18([0-3]|[5-9]))|(177))\\d{8}([^\\d])");
-        mobiles = mobiles.stream().map(x -> x.substring(1, x.length())).distinct().collect(Collectors.toList());
+        mobiles = mobiles.stream().map(x -> x.substring(1, x.length() - 1)).distinct().collect(Collectors.toList());
         List<Map<String, Object>> maps = new ArrayList();
         for (String mobile : mobiles) {
             Map<String, Object> map = new HashMap(0);
